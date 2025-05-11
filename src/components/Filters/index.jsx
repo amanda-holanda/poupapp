38bf16f11752052c478cc6f42af51f0a78b01862
amanda-holanda <amanda.holanda@jusbrasil.com.br>
2025-05-11 19:17:46 -0300

    ajuste para ficar em ordem alfabetica

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -8,32 +8,32 @@ const Filters = () => {
         <select>
           <option>Todos</option>
           <option>Aluguel/hipoteca</option>
-          <option>Condomínio</option>
-          <option>Contas de água, luz, gás</option>
-          <option>Internet/telefone</option>
-          <option>Plano de saúde</option>
-          <option>Seguros</option>
-          <option>Mensalidades escolares/faculdade</option>
-          <option>Transporte público/combustível</option>
-          <option>Financiamentos</option>
-          <option>Impostos</option>
-          <option>Supermercado/alimentação</option>
-          <option>Farmácia/medicamentos</option>
-          <option>Vestuário/calçados</option>
-          <option>Manutenção de casa/carro</option>
-          <option>Educação</option>
-          <option>Cuidados pessoais</option>
-          <option>Lazer/entretenimento</option>
-          <option>Viagens</option>
-          <option>Restaurantes/bares</option>
           <option>Assinaturas</option>
           <option>Compras por impulso</option>
-          <option>Presentes/doações</option>
-          <option>Hobbies/esportes</option>
-          <option>Reparos domésticos</option>
+          <option>Condomínio</option>
+          <option>Contas de água, luz, gás</option>
+          <option>Cuidados pessoais</option>
+          <option>Educação</option>
+          <option>Farmácia/medicamentos</option>
+          <option>Financiamentos</option>
           <option>Gastos médicos inesperados</option>
+          <option>Hobbies/esportes</option>
+          <option>Impostos</option>
+          <option>Internet/telefone</option>
+          <option>Lazer</option>
+          <option>Manutenção de casa/carro</option>
+          <option>Mensalidades escolares/faculdade</option>
           <option>Multas</option>
           <option>Outros</option>
+          <option>Plano de saúde</option>
+          <option>Presentes/doações</option>
+          <option>Reparos domésticos</option>
+          <option>Restaurantes/bares</option>
+          <option>Seguros</option>
+          <option>Supermercado/alimentação</option>
+          <option>Transporte público/combustível</option>
+          <option>Vestuário/calçados</option>
+          <option>Viagens</option>
         </select>
       </label>
       <label>
