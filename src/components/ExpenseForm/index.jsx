1ca9c10c1534d5550c837f0356acdac5d3f7f5e5
amanda-holanda <amanda.holanda@jusbrasil.com.br>
2025-05-11 19:11:24 -0300

    adição das options nos selects dos filtros

--- src/components/ExpenseForm/index.jsx
+++ src/components/ExpenseForm/index.jsx
@@ -19,7 +19,7 @@ const ExpenseForm = ({ onAddExpense }) => {
     // Limpar formulário
     setDescription('');
     setValue('');
-    setCategory('Outros');
+    setCategory('Selecione uma opção');
     setDate('');
   };
 
@@ -43,6 +43,33 @@ const ExpenseForm = ({ onAddExpense }) => {
         value={category}
         onChange={(e) => setCategory(e.target.value)}
       >
+        <option>Selecione uma opção</option>
+        <option>Aluguel/hipoteca</option>
+        <option>Condomínio</option>
+        <option>Contas de água, luz, gás</option>
+        <option>Internet/telefone</option>
+        <option>Plano de saúde</option>
+        <option>Seguros</option>
+        <option>Mensalidades escolares/faculdade</option>
+        <option>Transporte público/combustível</option>
+        <option>Financiamentos</option>
+        <option>Impostos</option>
+        <option>Supermercado/alimentação</option>
+        <option>Farmácia/medicamentos</option>
+        <option>Vestuário/calçados</option>
+        <option>Manutenção de casa/carro</option>
+        <option>Educação</option>
+        <option>Cuidados pessoais</option>
+        <option>Lazer/entretenimento</option>
+        <option>Viagens</option>
+        <option>Restaurantes/bares</option>
+        <option>Assinaturas</option>
+        <option>Compras por impulso</option>
+        <option>Presentes/doações</option>
+        <option>Hobbies/esportes</option>
+        <option>Reparos domésticos</option>
+        <option>Gastos médicos inesperados</option>
+        <option>Multas</option>
         <option>Outros</option>
       </select>
       <input
--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -7,6 +7,33 @@ const Filters = () => {
         Filtrar Categoria:
         <select>
           <option>Todos</option>
+          <option>Aluguel/hipoteca</option>
+          <option>Condomínio</option>
+          <option>Contas de água, luz, gás</option>
+          <option>Internet/telefone</option>
+          <option>Plano de saúde</option>
+          <option>Seguros</option>
+          <option>Mensalidades escolares/faculdade</option>
+          <option>Transporte público/combustível</option>
+          <option>Financiamentos</option>
+          <option>Impostos</option>
+          <option>Supermercado/alimentação</option>
+          <option>Farmácia/medicamentos</option>
+          <option>Vestuário/calçados</option>
+          <option>Manutenção de casa/carro</option>
+          <option>Educação</option>
+          <option>Cuidados pessoais</option>
+          <option>Lazer/entretenimento</option>
+          <option>Viagens</option>
+          <option>Restaurantes/bares</option>
+          <option>Assinaturas</option>
+          <option>Compras por impulso</option>
+          <option>Presentes/doações</option>
+          <option>Hobbies/esportes</option>
+          <option>Reparos domésticos</option>
+          <option>Gastos médicos inesperados</option>
+          <option>Multas</option>
+          <option>Outros</option>
         </select>
       </label>
       <label>
